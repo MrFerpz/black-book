07fc934eb7b194eaf2cb1ce1ee30e05998e9a303
Fred <leferpz@gmail.com>
2025-05-02 17:59:41 -0400

    Written homepage which maps posts, and an index router in frontend

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,11 +1,38 @@
-import React from "react"
+import axios from "axios"
+import { useEffect, useState } from "react";
 
-export default function HomePage() {
+interface Post {
+    id: number,
+    authorID: number,
+    createdAt: Date,
+    content: string
+}
 
+export default function Home() {
+    const [posts, setPosts] = useState<Post[] | []>([]);
+
+    async function getPosts() {
+        try {
+            const res = await axios.get("http://localhost:4000/api/posts");
+            const allPosts: Post[] = res.data
+            setPosts(allPosts)
+        } catch(err) {
+            console.log(err)
+        }
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+    
+    if (posts) {
     return (
-        <>
-            <div>Hello there</div>
-            <a href="/signup">Signup</a>
-        </>
+        posts.map((post: Post) => {
+            <div>{post.content}</div>
+        })
+    )}
+
+    else return (
+        <div>No posts yet.</div>
     )
 }
